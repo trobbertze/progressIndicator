8d1197465ea839229b3f40f67b6baaaed4adb2cb
Theo Robbertze <theo.robbertze@gmail.com>
2014-07-08 11:57:41 +0200

    Now actually working

--- src/progressIndicator.js
+++ src/progressIndicator.js
@@ -3,9 +3,9 @@ ProgressIndicator = function(options){
   require("famous/core/famous");
   var View             = require('famous/core/View');
   var Surface          = require('famous/core/Surface');
-  var Transform = require('famous/core/Transform');
-  var StateModifier = require('famous/modifiers/StateModifier');
-  var Transitionable = require('famous/transitions/Transitionable');
+  var Transform        = require('famous/core/Transform');
+  var StateModifier    = require('famous/modifiers/StateModifier');
+  var Transitionable   = require('famous/transitions/Transitionable');
   var SpringTransition = require('famous/transitions/SpringTransition');
 
   Transitionable.registerMethod('spring', SpringTransition);
@@ -16,22 +16,35 @@ ProgressIndicator = function(options){
   function _ProgressIndicator(options) {
     View.apply(this);
 
-    var surface = new Surface({
-      content: "<img src='img/ajax-loader.gif'>",
-      size: [100, 100]
+    this.background = new Surface({
+      size: [undefined, undefined],
+      properties: {
+        backgroundColor: "rgba(0,0,0, 0.2)"
+      }
     });
 
-    this.stateModifier = new StateModifier({
-      origin: [0.5, 0]
+    this.backgroundModifier = new StateModifier({
+      origin: [0,0],
+      transform: Transform.translate(0, -window.innerHeight - 200),
+      opacity: 0
     });
 
-    this.add(this.stateModifier).add(surface);
 
-    this.spring = {
-      method: 'spring',
-      period: 1000,
-      dampingRatio: 0.3
-    };
+    var spinner = new Surface({
+      content: "<img src='packages/progressIndicator/img/ajax-loader.gif'>",
+      size: [31, 31],
+      properties: {
+        zIndex: "100"
+      }
+    });
+
+    this.spinnerModifier = new StateModifier({
+      origin: [0.5, 0.5],
+      transform: Transform.translate(0, -window.innerHeight)
+    });
+
+    this.add(this.backgroundModifier).add(this.background);
+    this.add(this.spinnerModifier).add(spinner);
 
   }
   // ---------------------------------------------------------------------------
@@ -39,13 +52,41 @@ ProgressIndicator = function(options){
   _ProgressIndicator.prototype.constructor = _ProgressIndicator;
   // ---------------------------------------------------------------------------
   _ProgressIndicator.prototype.show = function() {
-    this.stateModifier.setTransform(
-      Transform.translate(0, 300, 0), this.spring
+    this.spinnerModifier.halt();
+
+    this.backgroundModifier.setTransform(
+      Transform.translate(0, 0, 0)
+    );
+
+    this.backgroundModifier.setOpacity(1);
+
+    this.spinnerModifier.setTransform(
+      Transform.translate(0, 0, 0),
+      {
+        method: 'spring',
+        period: 400,
+        dampingRatio: 0.3
+      }
     );
   };
   // ---------------------------------------------------------------------------
   _ProgressIndicator.prototype.hide = function() {
+    this.spinnerModifier.halt();
 
+    this.backgroundModifier.setOpacity(0);
+
+    this.backgroundModifier.setTransform(
+      Transform.translate(0, -window.innerHeight - 200, 0)
+    );
+
+    this.spinnerModifier.setTransform(
+      Transform.translate(0, -window.innerHeight, 0),
+      {
+        method: 'spring',
+        period: 500,
+        dampingRatio: 0.3
+      }
+    );
   };
   // ---------------------------------------------------------------------------
   return new _ProgressIndicator(options);
